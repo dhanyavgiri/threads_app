f17fb904a2e4506d637f76298d2aa8b725c7daa1
Venkatagiri <venkatagiri@Venkatagiris-MacBook-Air.local>
2025-09-29 12:11:33 +0530

    feat(threads_be): Implementing the BE apis to provide the data for threads app

---
+++ src/comments/comments.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Comment } from './schema/comment.schema';
+import { exec } from 'child_process';
+
+@Injectable()
+export class CommentsService {
+
+  constructor(@InjectModel(Comment.name) private commentModel: Model<Comment>) {}
+  
+  create(createCommentDto: CreateCommentDto) {
+    const createComment = this.commentModel.create({
+      text: createCommentDto.text,
+      parent: createCommentDto.parentId || null,
+      user: createCommentDto.userId
+    })
+    return createComment.then((doc) => {
+      return doc.populate(['user', 'parent'])
+    });
+  }
+
+  findAll() {
+    return this.commentModel.find().populate(['user', 'parent']).exec();
+  }
+
+  getTopLevelComments() {
+    return this.commentModel.find({
+      parent: null
+    }).populate(['user', 'parent']).sort({createdAt: -1}).exec();
+  }
+
+  getCommentsByParent(parentId: string) {
+    return this.commentModel.find({
+      parent: parentId
+    }).populate(['user', 'parent']).sort({createdAt: -1}).exec();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} comment`;
+  }
+
+  update(id: number, updateCommentDto: UpdateCommentDto) {
+    return `This action updates a #${id} comment`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} comment`;
+  }
+}
